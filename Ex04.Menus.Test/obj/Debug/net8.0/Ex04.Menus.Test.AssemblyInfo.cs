12da46818314bea9f4621a9087eb9851a0059026
Bar Bichachi <barbi2@mta.ac.il>
2024-07-07 20:53:18 +0300

    init windows

--- Ex04.Menus.Test/obj/Debug/net8.0/Ex04.Menus.Test.AssemblyInfo.cs
+++ Ex04.Menus.Test/obj/Debug/net8.0/Ex04.Menus.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex04.Menus.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4757c3e1136a590497c0b8ed5f82be01efe8a69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+545300345a69a8d825806f2e7c48a347c49c69f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex04.Menus.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex04.Menus.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
